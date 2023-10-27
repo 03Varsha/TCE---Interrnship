523f135c87a8c7bc5b3c53211aa0bc219df9811d
03Varsha <144646334+03Varsha@users.noreply.github.com>
2023-10-27 12:12:38 +0530

    Create Home.js

---
+++ Cafe/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <section className="home" id="home">
+        <div className="content">
+          <h3>
+            Intern's <span>Cafe</span>
+          </h3>
+          <p>
+            <h4>Order with ease - User Friendly Interface, Contactless payments, Pre-ordering method</h4>
+          </p>
+          <a href="#" className="btn">
+            Order now
+          </a>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
